86ae3b4621910955032a42c72c94fa1123ed0b4d
pqkkkk <22120174@student.hcmus.edu.vn>
2025-06-21 14:01:14 +0700

    Refactor video upload functionality and update API calls for YouTube integration

--- frontend/services/video_api.tsx
+++ frontend/services/video_api.tsx
@@ -29,62 +29,6 @@ export const CreateVideoApi = async (request : any) => {
     }
 }
 
-// Lấy url để đăng nhập Youtube -> oke
-export const getYoutubeAuthUrlApi = async (redirect_uri: string) => {
-    try {
-        const response = await api.get("/youtube/login", {
-            params: {redirect_uri}
-        });
-        return response.data.auth_url;
-    }
-    catch (error) {
-        console.log("Error get auth url youtube", error)
-        throw error;
-    }
-}
-
-// Lấy url để đăng nhập facebook -> 
-export const getFacebookthUrlApi = async (redirect_uri: string) => {
-    try {
-        const response = await api.get("/facebook/login-url", {
-            params: {redirect_uri}
-        });
-        return response.data.auth_url;
-    }
-    catch (error) {
-        console.log("Error get auth url youtube", error)
-        throw error;
-    }
-}
-// Lấy access token từ code trả về của đăng nhập youtube -> oke
-export const getYoutubeAccessToken = async (code: string, redirect_uri: string) =>  {
-    try {
-        const response = await api.get("/youtube/callback", {
-            params: {code, redirect_uri}
-        })
-        return response.data.access_token;
-    }
-    catch (error) {
-        console.log("error in get access token", error);
-        throw error;
-    }
-}
-
-// Lấy access token từ code của facebook
-export const getFacebookAccessToken = async (code: string, redirect_uri: string) =>  {
-    try {
-        const response = await api.get("/facebook/callback", {
-            params: {code, redirect_uri}
-        })
-        return response.data.access_token;
-    }
-    catch (error) {
-        console.log("error in get access token", error);
-        throw error;
-    }
-}
-
-
 // Search query -> oke
 export const searchTrendingVideos = async (keyword: string) => {
     try {
@@ -97,14 +41,9 @@ export const searchTrendingVideos = async (keyword: string) => {
 }
 
 // upload video
-export const uploadVideoApi = async (upload_video: any, youtubeToken: string) => {
+export const uploadVideoToYoutubeApi = async (request: any) => {
     try {
-        const response = await api.post("/upload_video",upload_video,
-            {headers: {
-                "youtube-token": youtubeToken, // ← đúng tên header backend mong đợi
-                }
-            }
-        )
+        const response = await api.post("/video/upload/to-youtube", request)
         return response.data
     }
     catch (error) {
